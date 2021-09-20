4fe024e4fd9eb01d2cf6aebb63325a64a1023b26
Isaias Ferreira <isayas_ferreira@hotmail.com>
2021-09-19 23:42:21 -0300

    Added new dadas in classe SellerService

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 
 namespace SalesWebMVC
 {
@@ -41,6 +42,7 @@ namespace SalesWebMVC
 
             //Adicionando um novo servico
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
