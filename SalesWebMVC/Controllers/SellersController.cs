7b333807cdee88692a0541b982e82ecc277ffe82
Isaias Ferreira <isayas_ferreira@hotmail.com>
2021-10-03 15:08:55 -0300

    Added new link to SellerService and created a new html for Create.cshtml.

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Services;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Controllers
 {
@@ -20,6 +21,19 @@ namespace SalesWebMVC.Controllers
         {
             var list = _sellerService.FindAll(); //Retorna a lista do banco.
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]  //Notação que o metodo é um POST
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); //Redireciona a pagina para o index novamente.
         }
     }
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -21,5 +21,12 @@ namespace SalesWebMVC.Services
         {
             return _context.Seller.ToList();
         }
+
+        //Metodo para inserir novos dados no banco de dados
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
