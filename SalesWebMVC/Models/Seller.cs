9775b74f338251292506d62076c32211d68255df
Isaias Ferreira <isayas_ferreira@hotmail.com>
2021-10-03 21:56:59 -0300

    It was formatted date, numbers and others character to show the user correctly.

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,19 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)] //Notation para trasformar o email em um link
         public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]//Notation para alterar o cabeçalho da tela do sellers
+        [DataType(DataType.Date)] //Notation para ignorar a hora
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")] //Notation para inverter a data para o padrao brasileiro        
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]//Notation para alterar o cabeçalho da tela do sellers
+        [DisplayFormat(DataFormatString = "{0:F2}")]//Notation para separa as casas decimais
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; } //Garante que o ID deve existir para nao ficar null.
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>(); //Inicializando a lista
