4ad4ae625888baa6b3cbfc6c340d8f28b445adfc
Isaias Ferreira <isayas_ferreira@hotmail.com>
2021-10-10 09:48:10 -0300

    Created validation to menus, like that - [Required(ErrorMessage = "{0} required")] - [EmailAddress(ErrorMessage = "Enter a valid email")] - [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -40,6 +40,14 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]  //Notação de seguranca quando recebe o token
         public IActionResult Create(Seller seller)
         {
+            //validação do javascrip no navegador
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index)); //Redireciona a pagina para o index novamente.
         }
@@ -113,6 +121,14 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]  //Notação de seguranca quando recebe o token
         public IActionResult Edit(int id, Seller seller)
         {
+            //validação do javascrip no navegador
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismach" });
--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,16 +9,24 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")] //Notação para validar o campo nome.
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")] //Notação para validar o campo nome.
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)] //Notation para trasformar o email em um link
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")] //Notação para validar o campo nome.
         [Display(Name = "Birth Date")]//Notation para alterar o cabeçalho da tela do sellers
         [DataType(DataType.Date)] //Notation para ignorar a hora
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")] //Notation para inverter a data para o padrao brasileiro        
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")] //Notação para validar o campo nome.
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]//Notation para alterar o cabeçalho da tela do sellers
         [DisplayFormat(DataFormatString = "{0:F2}")]//Notation para separa as casas decimais
         public double BaseSalary { get; set; }
